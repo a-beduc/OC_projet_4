0b16faa733911255a50ee7af1d2ed0ac9d566da2
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-09-15 14:18:38 +0200

    test of a new tournament from creation to finish

--- script_test.py
+++ script_test.py
@@ -1,64 +1,94 @@
 """This script was created to test the models found in /models"""
 from models.tournament import Tournament
-from models.player import Player
-from models.match import Match
-from models.round import Round
 
 
 def main():
-    # create a few Players
-    player_a = Player(last_name="Nakamura",
-                      first_name="Hikaru",
-                      date_of_birth="1986-12-09",
-                      chess_id="AA00008")
-
-    player_b = Player(last_name="Carlsen",
-                      first_name="Magnus",
-                      date_of_birth="1990-11-30",
-                      chess_id="AA00001")
-
-    player_c = Player(last_name="Nepomniachtchi",
-                      first_name="Ian",
-                      date_of_birth="1990-07-14",
-                      chess_id="AA00002")
-
-    player_d = Player(last_name="Ding",
-                      first_name="Liren",
-                      date_of_birth="1992-10-24",
-                      chess_id="AA00003")
-
-    list_players = [player_a, player_b, player_c, player_d]
-
-    print("------------------")
-    for player in list_players:
-        print(repr(player))
-    print("------------------")
-    print("\n")
-
-    # create a round and add players
-
-    round_1 = Round("Round y")
-    match_1 = Match(player_a.software_id,
-                    player_b.software_id)
-    match_2 = Match(player_c.software_id,
-                    player_d.software_id)
-    round_1.add_match([match_1.software_id, match_2.software_id])
-
-    round_1.add_match("m_16")
-    round_1.save_to_database()
-    print(f"Round 1 : {repr(round_1)}")
-    print(f"Match 1: {repr(match_1)}")
-    print(f"Match 2: {repr(match_2)}")
-
-    print("------------------")
-
-    tournament_1 = Tournament(name="Tournament test",
+    # Test new_tournament, do not handle draw yet, it was just to test a tournament
+    new_tou = Tournament(name="Tournament test",
                               place="Testland",
                               date_start="2020-12-04",
                               date_end="2020-12-05",
                               description="test of a description",
-                              rounds_number=8)
-    print(tournament_1)
+                              rounds_number=4)
+    list_of_players = ["p_1", "p_2", "p_3", "p_4", "p_5", "p_6"]
+    for player in list_of_players:
+        new_tou.add_participant(player)
+
+    new_tou.initialize_first_round()
+
+    print(new_tou.rounds['Round_1'].matches)
+
+    result = []
+    for match in new_tou.rounds['Round_1'].matches:
+        print(repr(match))
+        x = input('gagnant ? (p_x)')
+        result.append(x)
+
+    new_tou.rounds['Round_1'].get_match_by_player_id(result[0]).id_win(result[0])
+    new_tou.rounds['Round_1'].get_match_by_player_id(result[1]).id_win(result[1])
+    new_tou.rounds['Round_1'].get_match_by_player_id(result[2]).id_win(result[2])
+    new_tou.end_round('Round_1')
+    new_tou.start_next_round()
+    print('############')
+    print(new_tou.participants)
+    print(new_tou.get_ranking())
+    print(new_tou.rounds)
+    print('############')
+    new_tou.save_to_database()
+
+    result = []
+    for match in new_tou.rounds['Round_2'].matches:
+        print(repr(match))
+        x = input('gagnant ? (p_x)')
+        result.append(x)
+
+    new_tou.rounds['Round_2'].get_match_by_player_id(result[0]).id_win(result[0])
+    new_tou.rounds['Round_2'].get_match_by_player_id(result[1]).id_win(result[1])
+    new_tou.rounds['Round_2'].get_match_by_player_id(result[2]).id_win(result[2])
+    new_tou.end_round('Round_2')
+    new_tou.start_next_round()
+    print('############')
+    print(new_tou.participants)
+    print(new_tou.get_ranking())
+    print(new_tou.rounds)
+    print('############')
+    new_tou.save_to_database()
+
+    result = []
+    for match in new_tou.rounds['Round_3'].matches:
+        print(repr(match))
+        x = input('gagnant ? (p_x)')
+        result.append(x)
+
+    new_tou.rounds['Round_3'].get_match_by_player_id(result[0]).id_win(result[0])
+    new_tou.rounds['Round_3'].get_match_by_player_id(result[1]).id_win(result[1])
+    new_tou.rounds['Round_3'].get_match_by_player_id(result[2]).id_win(result[2])
+    new_tou.end_round('Round_3')
+    new_tou.start_next_round()
+    print('############')
+    print(new_tou.participants)
+    print(new_tou.get_ranking())
+    print(new_tou.rounds)
+    print('############')
+    new_tou.save_to_database()
+
+    result = []
+    for match in new_tou.rounds['Round_4'].matches:
+        print(repr(match))
+        x = input('gagnant ? (p_x)')
+        result.append(x)
+
+    new_tou.rounds['Round_4'].get_match_by_player_id(result[0]).id_win(result[0])
+    new_tou.rounds['Round_4'].get_match_by_player_id(result[1]).id_win(result[1])
+    new_tou.rounds['Round_4'].get_match_by_player_id(result[2]).id_win(result[2])
+    new_tou.end_round('Round_4')
+    print('############')
+    print(new_tou.participants)
+    print(new_tou.get_ranking())
+    print(new_tou.rounds)
+    print('############')
+    new_tou.save_to_database()
+
 
 
 
